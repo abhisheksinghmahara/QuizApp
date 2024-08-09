9f7f7a53b6f9ab2e99a3c9f3bdfb02c1e316f9f7
abhisheksinghmahara <abhisheksinghmahara@gmail.com>
2024-08-09 18:15:01 +0530

    Ready to Play

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 # Added some demo data
 # Added some styling and added some questions as hard coded data
 # Background color buttons and used prompt to get name of user
+# Next button styling
+# Quiz score counts and result shown at end
+# This quiz is ready to be played you can add some more quesions as well
--- quizJs.js
+++ quizJs.js
@@ -1,3 +1,4 @@
+// taking name input
 const entered_name=prompt("Enter name", "");
 document.getElementById("individual_name").innerHTML=entered_name;
 
@@ -9,7 +10,7 @@ function changeColor(newColor) {
     const elem = document.getElementById("body");
     elem.style.backgroundColor = newColor;
   }
-
+// set of questions 
 const questions = [
     {
         question: "Which is the largest desert in the world?",
@@ -59,3 +60,74 @@ const questions = [
 ]
 let currentQuestionIndex = 0;
 let score = 0;
+const startQuiz = () => {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+const showQuestion = () => {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + '.' + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer);
+    })
+}
+
+const resetState = () => {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+const selectAnswer = (e) => {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === 'true';
+    if (isCorrect) {
+        selectedBtn.classList.add('correct');
+        score++;
+    } else {
+        selectedBtn.classList.add('incorrect');
+    }
+    Array.from(answerButtons.children).forEach(button =>{
+        if (button.dataset.correct === 'true') {
+            button.classList.add('correct');
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = 'block';
+}
+const showScore=()=>{
+    resetState();
+    questionElement.innerHTML=`${entered_name}! You scored ${score*20} out of ${questions.length*20}`;
+    nextButton.innerHTML='Play Again';
+    nextButton.style.display='block';
+}
+const handleNextButton=()=>{
+    currentQuestionIndex++;
+    if(currentQuestionIndex<questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+nextButton.addEventListener('click',()=>{
+    if(currentQuestionIndex<questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
+console.log(score)
+startQuiz();
